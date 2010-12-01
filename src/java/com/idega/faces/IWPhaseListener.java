2583e0831ff2dde55b7fe5f2e8d1dfed8bc980e7
Pall Helgason <palli@idega.com>
2010-12-01 14:46:32 +0000

    Changing the location of the facesconfig dtd to our local server

--- src/java/com/idega/faces/IWPhaseListener.java
+++ src/java/com/idega/faces/IWPhaseListener.java
@@ -21,136 +21,138 @@ import com.idega.event.IWEventProcessor;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.PresentationObject;
 
+public class IWPhaseListener implements PhaseListener {
 
-public class IWPhaseListener implements PhaseListener{
+	private static Logger log = Logger.getLogger(IWPhaseListener.class
+			.getName());
 
-	private static Logger log = Logger.getLogger(IWPhaseListener.class.getName());
-	
-	/* (non-Javadoc)
-	 * @see javax.faces.event.PhaseListener#afterPhase(javax.faces.event.PhaseEvent)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.faces.event.PhaseListener#afterPhase(javax.faces.event.PhaseEvent)
 	 */
 	public void afterPhase(PhaseEvent arg0) {
-		callMainOnRoot(arg0.getFacesContext(),arg0.getFacesContext().getViewRoot());
+		callMainOnRoot(arg0.getFacesContext(), arg0.getFacesContext()
+				.getViewRoot());
 	}
-	
-	protected void callMainOnRoot(FacesContext context,UIViewRoot root){
+
+	protected void callMainOnRoot(FacesContext context, UIViewRoot root) {
 		log.finer("IWPhaseListener.callMainOnRoot()");
 		IWContext iwc = IWContext.getIWContext(context);
-		//recurseMain(iwc,root);
-		callMain(iwc,root);
+		// recurseMain(iwc,root);
+		callMain(iwc, root);
 	}
-	
+
 	/*
-	protected void recurseMain(IWContext iwc,UIComponent comp){
-		if(comp!=null){
-			if(comp instanceof PresentationObject){
-				PresentationObject po = (PresentationObject)comp;
+	 * protected void recurseMain(IWContext iwc,UIComponent comp){
+	 * if(comp!=null){ if(comp instanceof PresentationObject){
+	 * PresentationObject po = (PresentationObject)comp; try {
+	 * System.out.println("IWPhaseListener.recurseMain for "+po);
+	 * po.facesMain(iwc); } catch (Exception e) { // TODO Auto-generated catch
+	 * block e.printStackTrace(); } } List children = comp.getChildren(); for
+	 * (Iterator iter = children.iterator(); iter.hasNext();) { UIComponent
+	 * child = (UIComponent) iter.next(); recurseMain(iwc,child); } } }
+	 */
+
+	/**
+	 * This method goes down the tree to call the _main(iwc) methods of
+	 * PresentationObjects
+	 * 
+	 * @param iwc
+	 * @param comp
+	 */
+	protected void callMain(IWContext iwc, UIComponent comp) {
+		if (comp != null) {
+			// Class compClass = comp.getClass();
+			if (comp instanceof PresentationObject) {
+				// if(compClass.isAssignableFrom(PresentationObject.class)){
+				PresentationObject po = (PresentationObject) comp;
 				try {
-					System.out.println("IWPhaseListener.recurseMain for "+po);
-					po.facesMain(iwc);
+					// po._main(iwc);
+					po.callMain(iwc);
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			}
-			List children = comp.getChildren();
-			for (Iterator iter = children.iterator(); iter.hasNext();) {
-				UIComponent child = (UIComponent) iter.next();
-				recurseMain(iwc,child);
-			}
-		}
-	}*/
-
-	/**
-	 * This method goes down the tree to call the _main(iwc) methods of PresentationObjects
-	 * @param iwc
-	 * @param comp
-	 */
-	protected void callMain(IWContext iwc,UIComponent comp){
-		boolean enabled = true;
-		
-		if(enabled){
-			if(comp!=null){
-				//Class compClass = comp.getClass();
-				if(comp instanceof PresentationObject){
-				//if(compClass.isAssignableFrom(PresentationObject.class)){
-					PresentationObject po = (PresentationObject)comp;
-					try {
-						//po._main(iwc);
-						po.callMain(iwc);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					//Disabled because in change of callMain() method, that doesn't recurse self down the tree:
-					//findNextInstanceOfNotPresentationObject(iwc,po);
-					for (Iterator iter = po.getFacetsAndChildren(); iter.hasNext();) {
-						UIComponent child = (UIComponent) iter.next();
-						callMain(iwc,child);
-					}
-				}
-				else{
-					//List children = comp.getChildren();
-					//for (Iterator iter = children.iterator(); iter.hasNext();) {
-					try{
-						for (Iterator iter = comp.getFacetsAndChildren(); iter.hasNext();) {
-							UIComponent child = (UIComponent) iter.next();
-							callMain(iwc,child);
-						}
-					}
-					catch(Exception e){
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-	}
-	
-	/**
-	 * This method goes down the component tree to find a child that is not an instance
-	 * of PresentationObject and calls call_Main for those components.<br>
-	 * This is to handle if we have the case PresentationObject->UIComponent->PresentationObject 
-	 * in the tree hierarchy and make sure _main(iwc) is called for all PresentationObjects.
-	 * @param iwc
-	 * @param comp
-	 */
-	protected void findNextInstanceOfNotPresentationObject(IWContext iwc, UIComponent comp) {
-		if(comp!=null){
-			//Class compClass = comp.getClass();
-			if(comp instanceof PresentationObject){
-			//if(compClass.isAssignableFrom(PresentationObject.class)){
-				//List children = comp.getChildren();
-				//for (Iterator iter = children.iterator(); iter.hasNext();) {
-				for (Iterator iter = comp.getFacetsAndChildren(); iter.hasNext();) {
+				// Disabled because in change of callMain() method, that doesn't
+				// recurse self down the tree:
+				// findNextInstanceOfNotPresentationObject(iwc,po);
+				for (Iterator iter = po.getFacetsAndChildren(); iter.hasNext();) {
 					UIComponent child = (UIComponent) iter.next();
-					findNextInstanceOfNotPresentationObject(iwc,child);
+					callMain(iwc, child);
 				}
-			}
-			else{
-				//List children = comp.getChildren();
-				//for (Iterator iter = children.iterator(); iter.hasNext();) {
-				try{
-					for (Iterator iter = comp.getFacetsAndChildren(); iter.hasNext();) {
+			} else {
+				// List children = comp.getChildren();
+				// for (Iterator iter = children.iterator(); iter.hasNext();) {
+				try {
+					for (Iterator iter = comp.getFacetsAndChildren(); iter
+							.hasNext();) {
 						UIComponent child = (UIComponent) iter.next();
-						callMain(iwc,child);
+						callMain(iwc, child);
 					}
-				}
-				catch(Exception e){
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		
 	}
 
-	/* (non-Javadoc)
-	 * @see javax.faces.event.PhaseListener#beforePhase(javax.faces.event.PhaseEvent)
+	/**
+	 * This method goes down the component tree to find a child that is not an
+	 * instance of PresentationObject and calls call_Main for those components.<br>
+	 * This is to handle if we have the case
+	 * PresentationObject->UIComponent->PresentationObject in the tree hierarchy
+	 * and make sure _main(iwc) is called for all PresentationObjects.
+	 * 
+	 * @param iwc
+	 * @param comp
+	 */
+	protected void findNextInstanceOfNotPresentationObject(IWContext iwc,
+			UIComponent comp) {
+		if (comp != null) {
+			// Class compClass = comp.getClass();
+			if (comp instanceof PresentationObject) {
+				// if(compClass.isAssignableFrom(PresentationObject.class)){
+				// List children = comp.getChildren();
+				// for (Iterator iter = children.iterator(); iter.hasNext();) {
+				for (Iterator iter = comp.getFacetsAndChildren(); iter
+						.hasNext();) {
+					UIComponent child = (UIComponent) iter.next();
+					findNextInstanceOfNotPresentationObject(iwc, child);
+				}
+			} else {
+				// List children = comp.getChildren();
+				// for (Iterator iter = children.iterator(); iter.hasNext();) {
+				try {
+					for (Iterator iter = comp.getFacetsAndChildren(); iter
+							.hasNext();) {
+						UIComponent child = (UIComponent) iter.next();
+						callMain(iwc, child);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.faces.event.PhaseListener#beforePhase(javax.faces.event.PhaseEvent)
 	 */
 	public void beforePhase(PhaseEvent phase) {
-		log.finer("IWPhaseListener.beforePhase() : "+phase.getPhaseId().toString());
-		IWEventProcessor.getInstance().processAllEvents(IWContext.getIWContext(phase.getFacesContext()));
+		log.finer("IWPhaseListener.beforePhase() : "
+				+ phase.getPhaseId().toString());
+		IWEventProcessor.getInstance().processAllEvents(
+				IWContext.getIWContext(phase.getFacesContext()));
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.faces.event.PhaseListener#getPhaseId()
 	 */
 	public PhaseId getPhaseId() {
