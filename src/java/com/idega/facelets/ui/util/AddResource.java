bfd4bcc6baf64fe55355b1d54da40332082d35be
Valdas Žemaitis <valdas@idega.com>
2013-04-17 12:43:51 +0300

    Made possible to ignore resources that are provided in app property

--- src/java/com/idega/facelets/ui/util/AddResource.java
+++ src/java/com/idega/facelets/ui/util/AddResource.java
@@ -1,17 +1,21 @@
 package com.idega.facelets.ui.util;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.el.ValueExpression;
 import javax.faces.context.FacesContext;
 
+import com.idega.idegaweb.IWMainApplicationSettings;
 import com.idega.presentation.IWBaseComponent;
 import com.idega.presentation.IWContext;
 import com.idega.util.CoreConstants;
 import com.idega.util.PresentationUtil;
+import com.idega.util.StringUtil;
 
 /**
- * 
+ *
  * @author <a href="civilis@idega.com">Vytautas Čivilis</a>
  * @version $Revision: 1.4 $
  *
@@ -23,32 +27,30 @@ public class AddResource extends IWBaseComponent {
 	public static final String resourcePositionHeader = "header";
 	public static final String javascriptFileExt = ".js";
 	public static final String cssFileExt = ".css";
-	
-	private static final String resourcePathProperty= "resourcePath";
+
+	private static final String resourcePathProperty = "resourcePath";
 	private String resourcePath;
 	private String resourceLocation = resourcePositionHeader;
-	
+
 	public String getResourcePath() {
 		return resourcePath;
 	}
-	
+
 	public String getResourcePath(FacesContext context) {
-		
 		String resourcePath = getResourcePath();
-		
-		if(resourcePath == null) {
+
+		if (resourcePath == null) {
 			ValueExpression ve = getValueExpression(resourcePathProperty);
 			resourcePath = ve == null ? null : ve.getValue(context.getELContext()).toString();
 		}
 
 		setResourcePath(resourcePath);
-		
+
 		return resourcePath;
 	}
-	
+
 	public void setResourcePath(String resourcePath) {
-		
-		if(resourcePath != null)
+		if (resourcePath != null)
 			this.resourcePath = resourcePath;
 	}
 
@@ -59,28 +61,34 @@ public class AddResource extends IWBaseComponent {
 	public void setResourceLocation(String resourceLocation) {
 		this.resourceLocation = resourceLocation;
 	}
-	
+
 	@Override
 	public void encodeBegin(FacesContext context) throws IOException {
 		super.encodeBegin(context);
-		
+
 		String resourcePath = getResourcePath(context);
-		
-		if(resourcePath != null && !CoreConstants.EMPTY.equals(resourcePath)) {
-			
+
+		if (resourcePath != null && !CoreConstants.EMPTY.equals(resourcePath)) {
+			IWMainApplicationSettings settings = IWContext.getIWContext(context).getIWMainApplication().getSettings();
+			String disabledResources = settings.getProperty("disabled_facelet_resources");
+			if (!StringUtil.isEmpty(disabledResources)) {
+				List<String> resources = Arrays.asList(disabledResources.split(CoreConstants.COMMA));
+				if (resources.contains(resourcePath))
+					return;
+			}
+
 			String resourceLocation = getResourceLocation();
-			
-			if(resourceLocation == null || CoreConstants.EMPTY.equals(resourceLocation))
+			if (resourceLocation == null || CoreConstants.EMPTY.equals(resourceLocation))
 				resourceLocation = resourcePositionHeader;
-			
+
 			IWContext iwc = IWContext.getIWContext(context);
-			if(resourcePath.endsWith(javascriptFileExt))
+			if (resourcePath.endsWith(javascriptFileExt))
 				PresentationUtil.addJavaScriptSourceLineToHeader(iwc, resourcePath);
 			else if(resourcePath.endsWith(cssFileExt))
 				PresentationUtil.addStyleSheetToHeader(iwc, resourcePath);
 		}
 	}
-	
+
 	@Override
 	public Object saveState(FacesContext ctx) {
 		Object values[] = new Object[2];
@@ -88,7 +96,7 @@ public class AddResource extends IWBaseComponent {
 		values[1] = this.resourcePath;
 		return values;
 	}
-	
+
 	@Override
 	public void restoreState(FacesContext ctx, Object state) {
 		Object values[] = (Object[]) state;
